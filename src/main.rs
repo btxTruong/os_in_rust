b1c57bcb82a088f56dd37c829ada30b38a827243
Truong Bui <btxtruong@gmail.com>
2020-07-04 15:35:49 +0700

    Make simple kernel print text in screen

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+// will be called when panic eg. exit, break ...
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    loop {
+
+    }
+}
+
+// No mangle disable name mangling to ensure that the Rust compiler really output a function with
+// the name _start
+static TEXT: &[u8] = b"The first time";
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in TEXT.iter().enumerate() {
+        // every ascii char take 1 byte
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
+    loop {
+
+    }
 }
